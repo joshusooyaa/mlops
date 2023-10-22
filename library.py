7e84abda9fb6aca188621e857b0beacffca4b342
Josh <106642518+joshusooyaa@users.noreply.github.com>
2023-10-21 17:00:47 -0700

    Update library.py with random_state

--- library.py
+++ library.py
@@ -9,6 +9,8 @@ import subprocess
 import sys
 subprocess.call([sys.executable, '-m', 'pip', 'install', 'category_encoders'])  #replaces !pip install
 import category_encoders as ce
+from sklearn.neighbors import KNeighborsClassifier  
+from sklearn.metrics import f1_score  
 
 
 class CustomOHETransformer(BaseEstimator, TransformerMixin):
@@ -237,4 +239,25 @@ class CustomRobustTransformer(BaseEstimator, TransformerMixin):
     result = self.transform(df)
     return result
 
+
+def find_random_state(features_df, labels, n=200):
+  var = []  #collect test_error/train_error where error based on F1 score
+  model = KNeighborsClassifier(n_neighbors=5)  #instantiate with k=5.
+  
+  #2 minutes
+  for i in range(1, n):
+      train_X, test_X, train_y, test_y = train_test_split(features_df, labels, test_size=0.2, shuffle=True,
+                                                      random_state=i, stratify=labels)
+      model.fit(train_X, train_y)  #train model
+      train_pred = model.predict(train_X)           #predict against training set
+      test_pred = model.predict(test_X)             #predict against test set
+      train_f1 = f1_score(train_y, train_pred)   #F1 on training predictions
+      test_f1 = f1_score(test_y, test_pred)      #F1 on test predictions
+      f1_ratio = test_f1/train_f1          #take the ratio
+      var.append(f1_ratio)
+
+  rs_value = sum(var)/len(var)  #get average ratio value
+
+  rs = np.array(abs(var - rs_value)).argmin()  #find the index of the smallest value
+  return rs
 
