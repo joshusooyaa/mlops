54b39050dcbd6e40af812fb8c2acf59f8e61e703
Josh <106642518+joshusooyaa@users.noreply.github.com>
2023-11-04 22:31:21 -0700

    Update library.py with data setup helpers

--- library.py
+++ library.py
@@ -294,3 +294,31 @@ customer_transformer = Pipeline(steps=[
     ('scale_time spent', CustomRobustTransformer('Time Spent')), #from 5
     ('impute', KNNImputer(n_neighbors=5, weights="uniform", add_indicator=False)),
     ], verbose=True)
+
+
+def dataset_setup(original_table, label_column_name:str, the_transformer, rs, ts=.2):
+  features = original_table.drop(columns=label_column_name) 
+  labels = original_table[label_column_name].to_list()
+
+  x_train, x_test, y_train, y_test = train_test_split(features, labels, test_size=ts, shuffle=True,
+                                                    random_state=rs, stratify=labels)
+  
+  x_train_transformed = the_transformer.fit_transform(x_train, y_train)
+  x_test_transformed = the_transformer.transform(x_test)
+
+  x_train_numpy = x_train_transformed.to_numpy()
+  x_test_numpy = x_test_transformed.to_numpy()
+  y_train_numpy = np.array(y_train)
+  y_test_numpy = np.array(y_test)
+
+  return x_train_numpy, x_test_numpy, y_train_numpy, y_test_numpy
+
+
+def titanic_setup(titanic_table, transformer=titanic_transformer, rs=titanic_variance_based_split, ts=.2):
+  return dataset_setup(titanic_table, 'Survived', transformer, rs=rs, ts=ts)
+
+
+def customer_setup(customer_table, transformer=customer_transformer, rs=customer_variance_based_split, ts=.2):
+  return dataset_setup(customer_table, 'Rating', transformer, rs=rs, ts=ts)
+
+
