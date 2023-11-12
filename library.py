5511363b0a1bff5587cbaa12cfe3f5c4c6144100
Josh <106642518+joshusooyaa@users.noreply.github.com>
2023-11-11 17:52:31 -0800

    Update library.py with HalvingGridSearchCV

--- library.py
+++ library.py
@@ -14,6 +14,8 @@ from sklearn.metrics import f1_score
 from sklearn.model_selection import train_test_split
 from sklearn.metrics import precision_score, recall_score, f1_score, accuracy_score
 from sklearn.linear_model import LogisticRegressionCV
+from sklearn.experimental import enable_halving_search_cv
+from sklearn.model_selection import HalvingGridSearchCV
 
 
 class CustomOHETransformer(BaseEstimator, TransformerMixin):
@@ -347,6 +349,20 @@ def threshold_results(thresh_list, actuals, predicted):
 
   fancy_df = result_df.style.format(precision=2).set_properties(**properties).set_table_styles([headers])
   return (result_df, fancy_df)
+
+
+def halving_search(model, grid, x_train, y_train, factor=2, min_resources="exhaust", scoring='roc_auc'):
+  halving_cv = HalvingGridSearchCV(
+    model, grid,
+    scoring=scoring,
+    n_jobs=-1,
+    min_resources=min_resources,
+    factor=factor,
+    cv=5, random_state=1234,
+    refit=True,
+  )
+
+  return halving_cv.fit(x_train, y_train)
 
 
 
